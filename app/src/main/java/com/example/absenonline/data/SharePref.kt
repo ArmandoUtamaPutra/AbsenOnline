ad12c609842c44fd5cd2a3462fa1e7390941b155
ArmandoUtamaPutra <cronnosteam@gmail.com>
2019-07-20 10:29:21 +0700

    absen save

--- app/src/main/java/com/example/absenonline/data/SharePref.kt
+++ app/src/main/java/com/example/absenonline/data/SharePref.kt
@@ -6,6 +6,7 @@ import android.content.SharedPreferences
 class SharePref {
     internal  var mContext : Context
     private  var sharePref : SharedPreferences
+    val statusabsen: String = "Statusabsen"
     constructor(context: Context){
         mContext = context
         sharePref = mContext.getSharedPreferences("Nik", Context.MODE_PRIVATE)
@@ -20,5 +21,14 @@ class SharePref {
     }
     fun deleteAllSetting(){
         sharePref.edit().remove("nik").commit()
+    }
+
+    fun saveStatusAbsen(status:Boolean){
+    val editor = sharePref.edit()
+        editor.putBoolean(statusabsen,status)
+        editor.apply()
+    }
+    fun getStatusAbsen():Boolean{
+        return sharePref.getBoolean(statusabsen,false)
     }
 }
--- app/src/main/java/com/example/absenonline/view/DataAbsensi.kt
+++ app/src/main/java/com/example/absenonline/view/DataAbsensi.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
 import com.example.absenonline.R
+import com.example.absenonline.data.SharePref
 import kotlinx.android.synthetic.main.absen.*
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
@@ -12,19 +13,29 @@ import java.time.format.DateTimeFormatter
 class DataAbsensi : AppCompatActivity (){
 
     var oke : String? = null
+    var shared:SharePref? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.absen)
+        shared = SharePref(this)
         oke = getTimeOk()
         tanggalabsen.text = oke
 
+        val statusabsen = shared!!.getStatusAbsen()
+        if(statusabsen == true){
+            absenmasuk.visibility = View.GONE
+            absenpulang.visibility = View.VISIBLE
+        }else{
+            absenmasuk.visibility = View.VISIBLE
+            absenpulang.visibility = View.GONE
+        }
 
         absenmasuk.setOnClickListener {
             absenpulang.visibility = View.VISIBLE
             absenmasuk.visibility = View.GONE
             centang1.visibility = View.VISIBLE
             centang2.visibility = View.GONE
-
+            shared!!.saveStatusAbsen(true)
         }
         absenpulang.setOnClickListener {
             absenmasuk.visibility = View.GONE
